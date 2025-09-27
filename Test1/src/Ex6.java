390b60f2071611a0c76525f95a448e792a11e4f3
tudortepordei <151092250+tudortepordei@users.noreply.github.com>
2025-09-27 13:27:48 +0300

    Ex6

---
+++ Test1/src/Ex6.java
@@ -0,0 +1,14 @@
+public class Ex6 {
+
+    public static void main(String[] args) {
+        System.out.println("Numerele pare de la 1 la 100 sunt:");
+
+        for (int i = 1; i <= 100; i++) {
+            if (i % 2 == 0) {
+                System.out.print(i + " ");
+            }
+        }
+
+    }
+
+}
