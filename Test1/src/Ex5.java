6a1dcda32f7eb1e717efbafb1ed2808ab5736f48
tudortepordei <151092250+tudortepordei@users.noreply.github.com>
2025-09-27 13:24:58 +0300

    Ex5

---
+++ Test1/src/Ex5.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Ex5 {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Introdu parola: ");
+
+        int password = scanner.nextInt();
+        System.out.println("Parola este: "+ password);
+
+    }
+
+}
