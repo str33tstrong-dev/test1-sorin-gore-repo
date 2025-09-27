f4ee8f58e8d8cb49fc7e2945fcd935d2b7e5b595
tudortepordei <151092250+tudortepordei@users.noreply.github.com>
2025-09-27 13:12:54 +0300

    Ex2

---
+++ Test1/src/Ex2.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Ex2 {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        int primNumber = scanner.nextInt();
+        int secondNumber = scanner.nextInt();
+
+        System.out.println(primNumber * secondNumber);
+    }
+}
