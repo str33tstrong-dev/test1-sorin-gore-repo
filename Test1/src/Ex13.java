c53bbd77a563989fa9198710c8fb681e27e35ab9
tudortepordei <151092250+tudortepordei@users.noreply.github.com>
2025-09-27 14:04:03 +0300

    Ex13

---
+++ Test1/src/Ex13.java
@@ -0,0 +1,15 @@
+public class Ex13 {
+
+    public static void main(String[] args) {
+
+        for (int i = 1; i <= 20; i++) {
+            if (i == 14) {
+                break;
+            }
+            System.out.println(i);
+        }
+
+    }
+
+}
+
