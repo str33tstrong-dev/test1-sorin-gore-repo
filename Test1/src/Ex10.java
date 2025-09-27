74d32c1c703175108f446a16bd95dee290ca7fca
tudortepordei <151092250+tudortepordei@users.noreply.github.com>
2025-09-27 13:56:12 +0300

    Ex10

---
+++ Test1/src/Ex10.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Ex10 {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Introdu un text: ")
+        String text = scanner.next();
+        System.out.println("Textul modificat este:" + text.toUpperCase());
+
+    }
+
+}
