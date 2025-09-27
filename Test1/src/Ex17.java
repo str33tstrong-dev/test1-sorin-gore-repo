51be40b749436374f277e457f08c42b48637c250
tudortepordei <151092250+tudortepordei@users.noreply.github.com>
2025-09-27 14:19:13 +0300

    Ex17

---
+++ Test1/src/Ex17.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+import java.util.Arrays;
+
+public class Ex17 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Introdu numărul de elemente din array: ");
+        int n = scanner.nextInt();
+
+        int[] array = new int[n];
+
+        System.out.println("Introdu elementele array-ului:");
+        for (int i = 0; i < n; i++) {
+            System.out.print("Elementul " + i + ": ");
+            array[i] = scanner.nextInt();
+        }
+
+        int maxim = array[0];
+        for (int i = 1; i < array.length; i++) {
+            if (array[i] > maxim) {
+                maxim = array[i];
+            }
+        }
+
+        System.out.println("Array-ul: " + Arrays.toString(array));
+        System.out.println("Elementul cu valoarea cea mai mare este: " + maxim);
+
+    }
+}
